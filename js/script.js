d97626ac1f78579500623bacd8f49a47ebb61f19
Altukhov-Vladislav <90462643+Altukhov-Vladislav@users.noreply.github.com>
2021-12-19 20:07:08 +0300

    Update script.js
    изменения под мобильную версию

--- js/script.js
+++ js/script.js
@@ -56,6 +56,15 @@ $(document).ready(function () {
         pauseOnFocus: true,
         autoplaySpeed: 2500,
         speed: 1500,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                centerMode: false,
+                variableWidth: true,
+            }
+        }],
     });
 
     $('.partners__slider-2').slick({
@@ -68,5 +77,15 @@ $(document).ready(function () {
         pauseOnFocus: true,
         autoplaySpeed: 2700,
         speed: 2000,
+        responsive: [{
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                centerMode: true,
+                focusOnSelect: true,
+                variableWidth: true
+            }
+        }],
     });
 });
